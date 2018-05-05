60b5c0487f8310cc0757750f50c8de2e36917213
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-05-05 08:56:09 -0700

    pseudocoding process

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var fs = require("fs");
-var request = require("request-promise");
+const fs = require("fs");
+const request = require("request-promise");
 
 fs.readFile("drivers.json", "utf8", function(error, data) {
   // If the code experiences any errors it will log the error to the console.
@@ -9,17 +9,39 @@ fs.readFile("drivers.json", "utf8", function(error, data) {
 
   // We will then print the contents of data
 //   console.log(data);
+  
 });
 
+calculateDistance = () => {
+// compute distance from various drivers to package
+// push to an array & sort array closest to farther
+// output is a sorted array of closest drivers
+}
 
-request({
-  method: "POST",
-  uri: "https://backend-programming-challenge.herokuapp.com/driver/3/dispatch",
-  json: true,
-  body: {
-    "shipmentId​": "2352839523"
-  },
-  headers: {
-    "User-Agent": "Bolt Dispatch"
-  }
-}).then(console.log, console.log);
+
+dispatchRequest = () => {
+    request({
+      method: "POST",
+      uri: "https://backend-programming-challenge.herokuapp.com/driver/3/dispatch",
+      json: true,
+      body: {
+        "shipmentId​": "2352839523"
+      },
+      headers: {
+        "User-Agent": "Bolt Dispatch"
+      }
+    }, (error, response, body) => {
+        console.log("Body", body);
+        // if accepted, console.log("Driver __ accepted package __");
+        // else dispatchRequest to new driver
+    });
+    // .then(console.log, console.log);
+}
+
+// ******************* Main application
+// read drivers.json
+// read shipments.json
+// initiate shipment dispatching process, grab first package
+// calculateDistance(); 
+// dispatch to drivers();
+// if no acceptances, dispatch to next driver. 
