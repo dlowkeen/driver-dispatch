12cfc0cb46ba0ad2880dfb4415bf8ba58e71852a
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-05-05 09:28:45 -0700

    manipulating drivers.json object

--- index.js
+++ index.js
@@ -3,29 +3,37 @@ const request = require("request-promise");
 const geolib = require("geolib");
 
 fs.readFile("drivers.json", "utf8", function(error, data) {
-  // If the code experiences any errors it will log the error to the console.
-  if (error) {
-    return console.log(error);
-  }
-
-  // We will then print the contents of data
-//   console.log(data);
-  
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+        return console.log(error);
+    }
+    // We will then print the contents of data
+    //   console.log(data);
+    
 });
 
+
 calculateDistance = () => {
-// compute distance from various drivers to package
-let driverLocation = {"latitude": 34.048, "longitude": -118.302};
-let shipmentLocation = {"latitude": 34.0375, "longitude": -118.249};
+    const drivers = require("./drivers.json");
+    console.log(drivers[1].coordinates);
+    console.log(Object.keys(drivers).length);
+  // compute distance from various drivers to package
+  let driverDistanceArray = [];
+  for (let i = 1; i < Object.keys(drivers).length + 1; i++) {
+    console.log("Drivers", drivers[i].coordinates);
+  }
+  console.log(driverDistanceArray);
+  let driverLocation = {"latitude": 34.048, "longitude": -118.302};
+  let shipmentLocation = {"latitude": 34.0375, "longitude": -118.249};
 
-let distance = geolib.getDistance(
-  driverLocation,
-  shipmentLocation
-)
-console.log("shipment is " + distance + " meters away.");
+  let distance = geolib.getDistance(
+    driverLocation,
+    shipmentLocation
+  )
+  console.log("shipment is " + distance + " meters away.");
 
-// push to an array & sort array closest to farther
-// output is a sorted array of closest drivers
+  // push to an array & sort array closest to farther
+  // output is a sorted array of closest drivers
 }
 
 
